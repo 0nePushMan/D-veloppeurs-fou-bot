8ec166bbcdb1d28ec52a89795271ab933c64eeae
Nairi <tnairi06@outlook.fr>
2022-06-29 14:30:55 +0200

    [+] start bot

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+from googlesearch import search
+import requests
+
+from dotenv import load_dotenv
+load_dotenv()
+import os 
+
+token = os.getenv('token')
+spotify = os.getenv('spotify')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.channel.id == 943076081999679518 and message.content.startswith('/s'):
+            results = []
+            for j in search(message.content, num=10, stop=10, pause=2):
+                results.append(j)
+            await message.channel.send(results[0])
+
+        if message.channel.id == 983752718915108934 and not message.content.isupper():
+            await message.channel.send('UNIQUEMENT EN MAJUSCULES SVP')
+        if message.channel.id == 984434964126904370 and not message.content.startswith('https'):
+            await message.delete()
+            query = 'https://api.spotify.com/v1/search?q=' + message.content + '&type=track'
+            results = requests.get(query, headers={'Authorization': 'Bearer ' + spotify, "Accept": "application/json", "Content-Type": "application/json"}).json()
+            await message.channel.send(results['tracks']['items'][0]['external_urls']['spotify'])
+    
+
+client = MyClient()
+client.run(token)
