e7fb699768c094af1cee80730511eff5e13ab6a4
Nairi <tnairi06@outlook.fr>
2022-06-29 17:32:08 +0200

    make it better

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import discord
 import base64
-from googlesearch import search
+from googlesearch import search 
 import requests
 
 from dotenv import load_dotenv
@@ -9,9 +9,6 @@ load_dotenv()
 
 token = os.getenv('token')
 spotify = os.getenv('spotify')
-
-print(spotify)
-
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -25,13 +22,15 @@ class MyClient(discord.Client):
         if message.content == '/help': 
             commands = '<#991687399224655992> /d prompt\n <#943076081999679518> /s prompt\n <#990996145637564436> /s prompt\n <#984434964126904370> prompt'
             await message.channel.send(commands)
+            return
 
-        if (message.channel.id == 943076081999679518 or message.channel.id == 990996145637564436) and message.content.startswith('/s'):
-            await message.delete()
-            results = []
-            for j in search(message.content, num=10, stop=10, pause=2):
-                results.append(j)
-            await message.channel.send(results[0])
+        if (message.channel.id == 943076081999679518 or message.channel.id == 990996145637564436) and message.content.startswith('/'):
+            if message.content.startswith('/g'):
+                await message.delete()
+                results = []
+                for j in search(message.content, num=10, stop=10, pause=2):
+                    results.append(j)
+                await message.channel.send(results[0])
 
         if message.channel.id == 983752718915108934 and not message.content.isupper():
             await message.channel.send('UNIQUEMENT EN MAJUSCULES SVP')
@@ -48,7 +47,7 @@ class MyClient(discord.Client):
                 print(err)
                 raise
 
-        if message.channel.id == 991687399224655992 and message.content.startswith('/d'):
+        if message.channel.id == 991687399224655992 and message.content.startswith('/'):
             await message.channel.send('Veuillez patienter quelques minutes')
             results = requests.post('https://bf.dallemini.ai/generate', json={"prompt": message.content.replace('/d', '')}, headers={
                                     "Accept": "application/json", "Content-Type": "application/json"}).json()
@@ -58,7 +57,7 @@ class MyClient(discord.Client):
                 fh.write(x)
 
             await message.channel.send(message.author.mention, file=discord.File(r'./pictures/' + message.content + '.png'))
-        elif message.channel.id == 991687399224655992 and not message.content.startswith('/d'):
+        elif message.channel.id == 991687399224655992 and not message.content.startswith('/'):
             await message.delete()
 
 
